4f37d14c5d63b8f731ba305305e085fa63f8acf4
Meghana-VS <meghavs7022@gmail.com>
2025-05-21 08:06:09 +0530

    Binary search : Search insert position

---
+++ src/main/java/com/dsa/searchingAndSorting/SearchInsertPosition.java
@@ -0,0 +1,22 @@
+package com.dsa.searchingAndSorting;
+
+// https://leetcode.com/problems/search-insert-position/description/
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,6};
+        int target = 2;
+        System.out.println(searchInsert(arr, target));
+    }
+    public static int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int start = 0;
+        int end = n-1;
+        while (start <= end){
+            int mid = start + (end-start)/2;
+            if(target == nums[mid]) return mid;
+            if(target < nums[mid]) end = mid - 1;
+            else start = mid + 1;
+        }
+        return start;
+    }
+}
