f037da59afe958711ee5ae401bbf8fef4f120b96
Meghana-VS <meghavs7022@gmail.com>
2025-05-23 11:28:51 +0530

    Updated Search in 2D matrix

--- src/main/java/com/dsa/searchingAndSorting/searching/SearchIn2DMatrix_2.java
+++ src/main/java/com/dsa/searchingAndSorting/searching/SearchIn2DMatrix_2.java
@@ -5,16 +5,16 @@ import java.util.Arrays;
 public class SearchIn2DMatrix_2 {
     public static void main(String[] args) {
         int[][] a = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
+                {1, 2, 3, 4},
+                {4, 5, 6, 7},
+                {7, 8, 9, 10}
         };
         int target = 8;
         int[] ans = searchIn2D(a,target);
         System.out.println(Arrays.toString(ans));
     }
     public static int[] searchIn2D(int[][] a, int target){
-        int n = a[0].length;
+        int n = a.length;
         for(int i=0; i<n; i++){
             for(int j=0; j<a[i].length; j++){
                 if(a[i][j] == target){
