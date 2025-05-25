b607b5dcb398e2a1bae539217c807e3f1eced317
Meghana-VS <meghavs7022@gmail.com>
2025-05-25 19:32:42 +0530

    kth smallest element : optimized

--- src/main/java/com/dsa/searchingAndSorting/searching/KthSmallestElementInASortedMatrix.java
+++ src/main/java/com/dsa/searchingAndSorting/searching/KthSmallestElementInASortedMatrix.java
@@ -1,6 +1,5 @@
 package com.dsa.searchingAndSorting.searching;
 
-import java.util.*;
 
 // https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/description/
 public class KthSmallestElementInASortedMatrix {
@@ -26,13 +25,39 @@ public class KthSmallestElementInASortedMatrix {
 //        Collections.sort(list);
 //        return list.get(k-1);
     //******************** BETTER (TC - O(nmlog(k)), SC - O(k) ***********************
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                pq.add(matrix[i][j]);
-                if(pq.size() > k) pq.poll();
+//        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+//        for(int i=0; i<n; i++){
+//            for(int j=0; j<m; j++){
+//                pq.add(matrix[i][j]);
+//                if(pq.size() > k) pq.poll();
+//            }
+//        }
+//        return pq.poll();
+    //******************** Optimal (TC - O(n * log(max-min))) ***********************
+        int start = matrix[0][0], end = matrix[n-1][n-1];
+        while(start < end){
+            int mid = start + (end-start)/2;
+            int count = lessThanOrdEquals(matrix, mid);
+            if(count < k){
+                start = mid+1;
+            }else {
+                end = mid;
             }
         }
-        return pq.poll();
+        return start;
+    }
+    private static int lessThanOrdEquals(int[][] matrix, int target){
+        int count = 0;
+        int n = matrix.length;
+        int row = n-1, col = 0;
+        while(row >=0 && col < n){
+            if(matrix[row][col] <= target){
+                count += (row+1);
+                col++;
+            }else {
+                row--;
+            }
+        }
+        return count;
     }
 }
